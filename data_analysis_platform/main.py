72d7b598af6f2d139d94cd550a270f1e26b84d90
hibana2077 <hibana2077@gmail.com>
2024-11-09 20:29:03 +0800

    Add data analysis platform with Streamlit, Docker configuration, and example data

---
+++ data_analysis_platform/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+st.title("GradCompass Data Analysis Platform")
+
+df = pd.read_csv("./example.csv")
+
+#plot
+fig = px.parallel_categories(df)
+
+st.plotly_chart(fig)
