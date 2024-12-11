1f8146383e9b60785d1cfdaa05d95ce2fed027d9
hibana2077 <hibana2077@gmail.com>
2024-12-11 19:15:19 +0800

    Fix backend URL formatting in data retrieval function

--- src/web/mypages/datavis.py
+++ src/web/mypages/datavis.py
@@ -7,8 +7,8 @@ import os
 BACKEND_URL = os.getenv("BACKEND_URL", "localhost")
 
 def get_data():
-    # response = requests.get(f"http://{BACKEND_URL}:10000/data")
-    response = requests.get(f"{BACKEND_URL}:10000/test/data")
+    # response = requests.get(f"{BACKEND_URL}/data")
+    response = requests.get(f"{BACKEND_URL}/test/data")
     return response.json()
 
 st.set_page_config(layout="wide")
