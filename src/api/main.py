9a7f291b5719ac4a7d70642bc51cb7f7277fbb4f
hibana2077 <hibana2077@gmail.com>
2024-12-11 18:30:13 +0800

    Remove PostgreSQL database initialization and connection code in favor of MongoDB

--- src/api/main.py
+++ src/api/main.py
@@ -13,24 +13,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 # Database
-import psycopg2
 import pymongo
 
 # Database initialization
-
-## PostgreSQL
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
-conn = psycopg2.connect(
-    dbname=POSTGRES_DB,
-    user=POSTGRES_USER,
-    password=POSTGRES_PASSWORD,
-    host=POSTGRES_HOST,
-    port=POSTGRES_PORT
-)
 
 ## MongoDB
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
