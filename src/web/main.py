a5c26b38cc2a2278d6dbd204210da6a6c08d5cc8
hibana2077 <hibana2077@gmail.com>
2024-12-11 20:43:35 +0800

    Refactor main.py to update environment variable for backend URL and clean up unused code

--- src/web/main.py
+++ src/web/main.py
@@ -1,28 +1,9 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import requests
-
 import os
 
-API_HOST = os.getenv("API_HOST", "localhost")
-
-# def get_data():
-#     response = requests.get(f"http://{API_HOST}:10000/data")
-#     return response.json()
-
-# st.set_page_config(layout="wide")
-# st.title("GradCompass Data Analysis Platform")
-
-# df = pd.DataFrame(get_data())
-
-# #plot
-# fig = px.parallel_categories(df)
-
-# st.plotly_chart(fig)
+BACKEND_URL = os.getenv("BACKEND_URL", "localhost")
 
 pg = st.navigation([
-    # st.Page("page1.py", title="First page", icon=""),
     st.Page("mypages/datavis.py", title="研究所推甄分析", icon="📊"),
     st.Page("mypages/prediction.py", title="落點分析", icon="🎯"),
     st.Page("mypages/admin.py", title="管理者", icon="🔒"),
