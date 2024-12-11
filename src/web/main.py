e732875f60434e1430aeb84157d09b7a3029f363
hibana2077 <hibana2077@gmail.com>
2024-12-11 18:57:19 +0800

    Refactor main.py to comment out unused code and implement navigation for new pages

--- src/web/main.py
+++ src/web/main.py
@@ -7,16 +7,23 @@ import os
 
 API_HOST = os.getenv("API_HOST", "localhost")
 
-def get_data():
-    response = requests.get(f"http://{API_HOST}:10000/data")
-    return response.json()
+# def get_data():
+#     response = requests.get(f"http://{API_HOST}:10000/data")
+#     return response.json()
 
-st.set_page_config(layout="wide")
-st.title("GradCompass Data Analysis Platform")
+# st.set_page_config(layout="wide")
+# st.title("GradCompass Data Analysis Platform")
 
-df = pd.DataFrame(get_data())
+# df = pd.DataFrame(get_data())
 
-#plot
-fig = px.parallel_categories(df)
+# #plot
+# fig = px.parallel_categories(df)
 
-st.plotly_chart(fig)
+# st.plotly_chart(fig)
+
+pg = st.navigation([
+    # st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page("mypages/prediction.py", title="落點分析", icon="🔥"),
+    st.Page("mypages/admin.py", title="管理者", icon="🔥"),
+])
+pg.run()
